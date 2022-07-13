59d0be77db9143ebbc1c93d8674a616d30ffcb3f
NemanjaMihajlovicHTEC <105635954+NemanjaMihajlovicHTEC@users.noreply.github.com>
2022-07-13 15:41:51 +0200

    code after EditButton click();

--- src/main/java/com/skynest/uitesting/pages/UserInfoPage.java
+++ src/main/java/com/skynest/uitesting/pages/UserInfoPage.java
@@ -25,6 +25,10 @@ public class UserInfoPage extends BasePage {
     private static final String EDIT_USER_LOCATOR = "//*[@id=\"root\"]/section/div/div/div[1]/div[1]/div/div[2]/button[1]";
     private static final String UPDATE_USER_LOCATOR = "//button[contains(text(),'Update')]";
     private static final String EDIT_USER_FIRST_NAME_LOCATOR = "//body/div[@id='root']/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]";
+    private static final String EDIT_USER_LAST_NAME_LOCATOR = "//body/div[@id='root']/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/input[1]";
+    private static final String EDIT_PHONE_NUMBER_LOCATOR = "//body/div[@id='root']/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[4]/div[2]/input[1]";
+    private static final String EDIT_COMPANY_POSITION_LOCATOR = "//body/div[@id='root']/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[5]/div[2]/input[1]";
+    private static final String EDIT_ADDRESS_LOCATOR = "//body/div[@id='root']/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[6]/div[2]/input[1]";
 
      private WebElement userDropDown;
      private WebElement yourProfile;
@@ -34,6 +38,12 @@ public class UserInfoPage extends BasePage {
      private WebElement logOut;
      private WebElement editButton;
      private WebElement updateButton;
+
+     private WebElement editFirstNameInputField;
+     private WebElement editLastNameInputField;
+     private WebElement editPhoneNumberInputField;
+     private WebElement editCompanyPositionInputField;
+     private WebElement editAddressInputField;
 
     public static WebElement editUserInfoDropdownButton(WebDriver driver) {
         //WebElement button = driver.findElement(By.xpath("///button[normalize-space()='Edit']"));
@@ -56,9 +66,18 @@ public class UserInfoPage extends BasePage {
         return fillEditedForm;
     }
 
+    public void waitForElement(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
     public void  selectUserOptions() {
-        userDropDown = driver.findElement(By.xpath(USER_DROPDOWN_LOCATOR));
-        userDropDown.click();
+        //waitForElement(By.xpath(USER_DROPDOWN_LOCATOR));
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(USER_DROPDOWN_LOCATOR)));
+//        userDropDown = driver.findElement(By.id(USER_DROPDOWN_LOCATOR));
+//        userDropDown.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("dropdown-menu-align-end")));
+        WebElement e = driver.findElement(By.id("dropdown-menu-align-end"));
+        e.click();
         yourProfile = driver.findElement(By.xpath(YOUR_PROFILE_LOCATOR));
         yourProfile.click();
     }
@@ -75,8 +94,49 @@ public class UserInfoPage extends BasePage {
     }
 
     public void clickClearAndFillFirstName() {
-
+        editFirstNameInputField = driver.findElement(By.xpath(EDIT_USER_FIRST_NAME_LOCATOR));
+        editFirstNameInputField.click();
+        editFirstNameInputField.sendKeys("John");
     }
+
+    public void clickClearAndFillLastName() {
+        editLastNameInputField = driver.findElement(By.xpath(EDIT_USER_LAST_NAME_LOCATOR));
+        editLastNameInputField.click();
+        editLastNameInputField.sendKeys("Wick");
+    }
+
+    public void clickClearAndFillPhoneNumber() {
+        editPhoneNumberInputField = driver.findElement(By.xpath(EDIT_PHONE_NUMBER_LOCATOR));
+        editPhoneNumberInputField.click();
+        editPhoneNumberInputField.sendKeys("06600118852");
+    }
+
+    public void clickClearAndFillCompanyPosition() {
+//        editCompanyPositionInputField = driver.findElement(By.xpath(EDIT_COMPANY_POSITION_LOCATOR));
+//        editCompanyPositionInputField.click();
+//        editCompanyPositionInputField.sendKeys("Quality Assurance");
+        wait.until(ExpectedConditions.visibilityOfElementLocated (By.xpath(EDIT_COMPANY_POSITION_LOCATOR)));
+        WebElement m = driver.findElement(By.xpath(EDIT_COMPANY_POSITION_LOCATOR));
+        m.click();
+        m.sendKeys("Quality Assurance");
+    }
+
+    public void clickClearAndFillAddressField() {
+//        editAddressInputField = driver.findElement(By.xpath(EDIT_ADDRESS_LOCATOR));
+//        editAddressInputField.click();
+//        editAddressInputField.sendKeys("Street 12");
+        wait.until(ExpectedConditions.visibilityOfElementLocated (By.xpath(EDIT_ADDRESS_LOCATOR)));
+        WebElement m = driver.findElement(By.xpath(EDIT_ADDRESS_LOCATOR));
+        m.click();
+        m.sendKeys("Street Address 12");
+    }
+
+    public void clickUpdateAfterEditing() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(UPDATE_USER_LOCATOR)));
+        updateButton = driver.findElement(By.xpath(UPDATE_USER_LOCATOR));
+        updateButton.click();
+    }
+
 
 //    private By newEditButton = By.xpath("//button[contains(text(),'Edit')]");
 //    public void waitForElement(By element) {
--- src/test/java/UserEditInfoTest.java
+++ src/test/java/UserEditInfoTest.java
@@ -1,16 +1,19 @@
 import com.skynest.uitesting.models.EditedUser;
 import com.skynest.uitesting.pages.EditUserInfoForm;
 import com.skynest.uitesting.pages.UserInfoPage;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import static com.skynest.uitesting.constants.PageUrlConstants.USER_PROFILE_URL;
 
 public class UserEditInfoTest extends BaseTest {
 
-    UserInfoPage selectOptions = new UserInfoPage(driver, wait);
+
 
     @Test()
     public void editing_users_details() throws InterruptedException {
+
+        UserInfoPage selectOptions = new UserInfoPage(driver, wait);
 
         login();
         selectOptions.selectUserOptions();
@@ -24,16 +27,25 @@ public class UserEditInfoTest extends BaseTest {
 //            }
 //        }
 
-        EditedUser editedUser = EditedUser.generateModifiedData();
-        EditUserInfoForm editUserInfoForm = userInfoPage.fillEditedForm()
-                .withFirstName(editedUser.getFirstName())
-                .withLastName(editedUser.getLastName())
-                .withPhoneNumber(editedUser.getPhoneNumber())
-                .withCompanyPosition(editedUser.getCompanyPosition())
-                .withAddress(editedUser.getAddress());
-        userInfoPage.scroll(editUserInfoForm.editUserInfoButton);
-        editUserInfoForm.submitEditedForm();
-        Assert.assertEquals(getCurrentUrl(), USER_PROFILE_URL);
+        selectOptions.clickClearAndFillFirstName();
+        selectOptions.clickClearAndFillLastName();
+        selectOptions.clickClearAndFillPhoneNumber();
+        selectOptions.clickClearAndFillCompanyPosition();
+        selectOptions.clickClearAndFillAddressField();
+        selectOptions.clickUpdateAfterEditing();
+        //Assert.assertEquals(driver.findElement(By.xpath("//body/div[@id='root']/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]")), "John Wick");
+        //Thread.sleep(3000);
+
+//        EditedUser editedUser = EditedUser.generateModifiedData();
+//        EditUserInfoForm editUserInfoForm = userInfoPage.fillEditedForm()
+//                .withFirstName(editedUser.getFirstName())
+//                .withLastName(editedUser.getLastName())
+//                .withPhoneNumber(editedUser.getPhoneNumber())
+//                .withCompanyPosition(editedUser.getCompanyPosition())
+//                .withAddress(editedUser.getAddress());
+//        userInfoPage.scroll(editUserInfoForm.editUserInfoButton);
+//        editUserInfoForm.submitEditedForm();
+        //Assert.assertEquals(getCurrentUrl(), USER_PROFILE_URL);
     }
 }
 
